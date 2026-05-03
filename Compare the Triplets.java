8ef303dee70847f1a1e9d31c7567da51bda395ee
Adarsh Ranjan <122526796+adarshranjan36@users.noreply.github.com>
2026-05-04 00:56:37 +0530

    Add compareTriplets function in Compare the Triplets.java
    Implement compareTriplets function to compare scores of two players.

---
+++ Compare the Triplets.java
@@ -0,0 +1,25 @@
+class Result {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+      int alice=0,bob=0;
+      for(int i=0;i<3;i++){
+        if(a.get(i)>b.get(i)){
+          alice++;
+        }
+        else if(a.get(i)<b.get(i)){
+          bob++;
+        }
+      }
+      return Arrays.asList(alice,bob);
+    }
+
+}
