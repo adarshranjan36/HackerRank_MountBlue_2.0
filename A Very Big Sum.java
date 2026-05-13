a4215cccb459ee65158011454c17132ee49b2581
Adarsh Ranjan <122526796+adarshranjan36@users.noreply.github.com>
2026-05-14 01:18:47 +0530

    Implement aVeryBigSum function to calculate sum

---
+++ A Very Big Sum.java
@@ -0,0 +1,19 @@
+class Result {
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<Long> ar) {
+      long sum=0;
+      for(long lg:ar){
+        sum+=lg;
+      }
+      return sum;
+    }
+    
+
+}
